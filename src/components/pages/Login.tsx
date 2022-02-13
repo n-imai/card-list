4be34b90a68a46cf6424abc3041553ba3a133bec
Nobuo Imai <8196611+n-imai@users.noreply.github.com>
2022-02-13 11:51:34 +0900

    create component partitioning

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {memo, VFC} from "react";
-import {Box, Button, Divider, Flex, Heading, Input, Stack} from "@chakra-ui/react";
+import {Box, Divider, Flex, Heading, Input, Stack} from "@chakra-ui/react";
+
+import { PrimaryButton } from "../atoms/button/PrimaryButton"
 
 export const Login: VFC = memo(() => {
   return (
@@ -10,7 +12,7 @@ export const Login: VFC = memo(() => {
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
           <Input placeholder="ユーザID" />
-          <Button bg="teal.400" color="white" _hover={{opacity: 0.8}}>ログイン</Button>
+          <PrimaryButton>ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
