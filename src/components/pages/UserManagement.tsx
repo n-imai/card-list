d39150561fe1a9c8616a83872039233637a6d6a0
Nobuo Imai <8196611+n-imai@users.noreply.github.com>
2022-02-13 16:58:38 +0900

    create admin feature

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -19,7 +19,7 @@ export const UserManagement: VFC = memo(() => {
   const {onSelectUser, selectedUser} = useSelectUser();
   const { loginUser } = useLoginUser();
 
-  useEffect(() => getUsers() ,[]);
+  useEffect(() => getUsers() ,[getUsers]);
 
   const onClickUser = useCallback(
     (id: number) => {
@@ -47,7 +47,12 @@ export const UserManagement: VFC = memo(() => {
         ))}
       </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose}/>
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+      />
     </>
   );
 });
