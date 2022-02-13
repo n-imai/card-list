bc39a8620ac8a11cd5e26fe104759d503f0360d0
Nobuo Imai <8196611+n-imai@users.noreply.github.com>
2022-02-13 12:56:40 +0900

    create user card component

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import {memo, VFC} from "react";
+import {Wrap, WrapItem} from "@chakra-ui/react";
+import {UserCard} from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザ管理ページです</p>
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="ユーザ名"
+          fullName="フルネーム"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
