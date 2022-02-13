b2b9b0f210ffa9bec8f7fdda2b1328c975802bfb
Nobuo Imai <8196611+n-imai@users.noreply.github.com>
2022-02-13 12:31:57 +0900

    useToast

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,11 @@ import {useCallback, useState} from "react";
 import axios from "axios";
 import {User} from "../types/api/user";
 import {useHistory} from "react-router-dom";
+import {useMessage} from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const {showMessage} = useMessage();
 
   const [loading, setLoading] = useState(false);
 
@@ -13,11 +15,17 @@ export const useAuth = () => {
 
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
       if (res.data) {
+        showMessage({title:"ログインしました", status:"success"});
         history.push("/home");
       }else{
-        alert("ユーザが見つかりません")
+        showMessage({title:"ユーザが見つかりません", status:"error"});
       }
-    }).catch(() => alert("ログインできません")).finally(() => setLoading(false));
-  }, [history]);
+    })
+      .catch(() => {
+          showMessage({title:"ログインできません", status:"error"});
+        }
+      )
+      .finally(() => setLoading(false));
+  }, [history, showMessage]);
   return { login, loading };
 }
